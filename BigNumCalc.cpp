dc1bf38e4cb3962eaf878e4d3502f1a40b65a077
Aegizz <lloyddraysey@gmail.com>
2024-04-19 00:15:15 +0930

    bigCalc

---
+++ BigNumCalc.cpp
@@ -0,0 +1,87 @@
+#include "BigNumCalc.h"
+
+
+
+bigNumCalc::bigNumCalc(){
+
+}
+bigNumCalc::~bigNumCalc(){
+
+}
+
+
+std::list<int> bigNumCalc::buildBigNum(std::string numString){
+    std::list<int> num;
+    for (int i = 0; i < numString.length(); i++){
+        num.push_back(numString[i] - '0');
+    }
+    return num;
+}
+std::list<int> bigNumCalc::add(std::list<int> num1, std::list<int> num2){
+    std::list<int> res;
+    int carry = 0;
+    while (!num1.empty() && !num2.empty() || carry){
+        int temp = carry;
+        if (!num1.empty()){
+            temp+= num1.back();
+            num1.pop_back();
+        }
+        if (!num2.empty()){
+            temp+=num2.back();
+            num2.pop_back();
+        }
+        res.push_front(temp % 10);
+        carry = temp/10;
+    }
+    return res;
+}
+std::list<int> bigNumCalc::sub(std::list<int> num1, std::list<int> num2){
+    std::list<int> res;
+    int borrow = 0;
+    while (!num1.empty() && !num2.empty()){
+        int sub = borrow;
+        if (num1.empty()){
+            sub+=num1.back();
+            num1.pop_back();
+        }
+        if (!num2.empty()){
+            sub-=num2.back();
+            num2.pop_back();
+        }
+        if (sub < 0){
+            sub +=10;
+            borrow = -1;
+        } else{
+            borrow= 0;
+        }
+        res.push_front(sub);
+
+    }
+    while (!res.empty() && res.front() == 0){
+        res.pop_front();
+    }
+    return res;
+}
+std::list<int> bigNumCalc::mul(std::list<int> num1, std::list<int> num2){
+    int digit = num2.front();
+    num2.pop_front();
+
+    std::list<int> result;
+    int carry = 0;
+
+    while (!num1.empty() || carry) {
+        int prod = carry;
+        if (!num1.empty()) {
+            prod += num1.back() * digit;
+            num1.pop_back();
+        }
+        result.push_front(prod % 10);
+        carry = prod / 10;
+    }
+
+    while (!result.empty() && result.front() == 0) {
+        result.pop_front();
+    }
+
+    return result;
+}
---
+++ BigNumCalc.h
@@ -0,0 +1,18 @@
+#ifndef BIGNUMCALC_H
+#define BIGNUMCALC_H
+
+#include <iostream>
+#include <list>
+#include <string>
+class bigNumCalc{
+    public:
+        bigNumCalc();
+        ~bigNumCalc();
+        std::list<int> buildBigNum(std::string numString);
+        std::list<int> add(std::list<int> num1, std::list<int> num2);
+        std::list<int> sub(std::list<int> num1, std::list<int> num2);
+        std::list<int> mul(std::list<int> num1, std::list<int> num2);
+
+};
+
+#endif
