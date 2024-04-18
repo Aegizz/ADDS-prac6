9c636426b94087585c93d06e788d9c45bb53f4cf
Aegizz <lloyddraysey@gmail.com>
2024-04-19 00:59:05 +0930

    maybe this work for zero edge case?

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -66,20 +66,13 @@ std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2){
     int digit = num2.front();
     num2.pop_front();
 
-    std::list<int> power;
     std::list<int> res;
+    res.push_back(0);
 
-    power.push_front(0);
-
-    while (!num1.empty()){
-        int amount = 0;
-        if (!num1.empty()){
-            amount += (num1.back() * digit);
-        }
-        res.push_front(amount % 10);
-        power.push_front(amount/10);
+    for (int i = 0; i < digit; i++){
+        res = this->add(res, num1);
     }
 
 
-    return add(res, power);
+    return res;
 }
